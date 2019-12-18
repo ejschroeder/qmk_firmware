7de48f3eeba5576a5ee247ab010d9f0db93a3417
E.J. Schroeder <ej.schr@gmail.com>
2019-12-17 22:26:01 -0500

    Rework arrowkeys, symbols, add mouse keys

--- keyboards/ergodox_ez/keymaps/ejschroeder/keymap.c
+++ keyboards/ergodox_ez/keymaps/ejschroeder/keymap.c
@@ -7,48 +7,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
   [BASE] = LAYOUT_ergodox(
     // Left Hand
-    KC_GRV,   KC_1,     KC_2,     KC_3,   KC_4,   KC_5,   KC_ESC, 
-    KC_TAB,   KC_Q,     KC_W,     KC_E,   KC_R,   KC_T,   KC_LBRC, 
-    KC_ESC,   KC_A,     KC_S,     KC_D,   KC_F,   KC_G, 
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,   KC_B,   KC_LPRN, 
-    KC_LCTL,  KC_LGUI,  KC_LALT,  OSL(1), MO(1),
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   KC_LT, 
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   KC_LBRC, 
+    KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,   KC_G, 
+    KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   KC_EQUAL, 
+              KC_LCTL,  KC_LGUI,  KC_LALT,  OSL(1), MO(1),
 
                                                         C(KC_C),  C(KC_V), 
                                                                   KC_HOME, 
                                               KC_SPC,   KC_BSPC,  KC_END, 
     // Right Hand
-    KC_EQL,   KC_6,     KC_7,   KC_8,     KC_9,     KC_0,     KC_MINS, 
+    KC_GT,    KC_6,     KC_7,   KC_8,     KC_9,     KC_0,     TG(1), 
     KC_RBRC,  KC_Y,     KC_U,   KC_I,     KC_O,     KC_P,     KC_BSLS, 
-    KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,  KC_QUOT, 
-    KC_RPRN,  KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, 
-    KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  TG(1), 
+              KC_H,     KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT, 
+    KC_MINUS, KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC, 
+              MO(1),    OSL(1), KC_RALT,  KC_RGUI,  KC_RCTL, 
     
     C(KC_X),  C(KC_SLSH), 
     KC_PGUP, 
-    KC_PGDN,  KC_DEL,   LCTL_T(KC_ENTER)
+    KC_PGDN,  LSFT_T(KC_DEL), LCTL_T(KC_ENTER)
   ),
 
 	[UTIL] = LAYOUT_ergodox(
     // Left Hand
-    XXXXXXX,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11, 
-    XXXXXXX,    KC_INS,   KC_HOME,  KC_UP,    KC_END,   KC_PSCR,  XXXXXXX, 
-    KC_CAPS,    XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_RGHT,  XXXXXXX, 
-    XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, 
-    XXXXXXX,    XXXXXXX,  XXXXXXX,  KC_TRNS,  KC_TRNS,
+    XXXXXXX,    KC_F1,    KC_F2,      KC_F3,    KC_F4,      KC_F5,        KC_F11, 
+    XXXXXXX,    XXXXXXX,  KC_MS_BTN1, KC_MS_U,  KC_MS_BTN2, KC_MS_WH_UP,  XXXXXXX, 
+    KC_CAPS,    XXXXXXX,  KC_MS_L,    KC_MS_D,  KC_MS_R,    KC_MS_WH_DOWN, 
+    XXXXXXX,    XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,    XXXXXXX,      XXXXXXX, 
+                XXXXXXX,  XXXXXXX,    XXXXXXX,  KC_TRNS,    KC_TRNS,
     
                                                         XXXXXXX,  XXXXXXX, 
                                                                   XXXXXXX, 
                                               XXXXXXX,  XXXXXXX,  XXXXXXX, 
     // Right Hand                                          
-    KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   XXXXXXX, 
-    XXXXXXX,  KC_NLCK,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,  KC_PSLS, 
-    XXXXXXX,  KC_P4,    KC_P5,    KC_P6,    KC_PCMM,  KC_PAST, 
-    XXXXXXX,  XXXXXXX,  KC_P1,    KC_P2,    KC_P3,    KC_PEQL,  KC_PMNS, 
-    KC_P0,    XXXXXXX,  KC_PDOT,  KC_PENT,  KC_TRNS, 
+    KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS, 
+    XXXXXXX,  XXXXXXX,  KC_HOME,  KC_UP,    KC_END,   XXXXXXX,  XXXXXXX, 
+              XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_RGHT,  XXXXXXX,  XXXXXXX, 
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, 
+              KC_TRNS,  KC_TRNS,  XXXXXXX,  XXXXXXX,  XXXXXXX, 
     
     KC_MPRV,  KC_MNXT, 
     KC_VOLU, 
     KC_VOLD,  KC_MPLY,  XXXXXXX
   )
-
 };
